a1efb5376f066378b9b75fa2189ee9ce9a369d76
Vitalii Morgunov <utlab13@gmail.com>
2022-08-25 21:39:38 +0200

    Update sui

--- sui_update.sh
+++ sui_update.sh
@@ -8,7 +8,7 @@ cd $HOME/sui
 git remote add upstream https://github.com/MystenLabs/sui
 git fetch upstream
 git checkout -B devnet --track upstream/devnet
-cargo build --release -p sui-node
+cargo build -p sui-node -p sui --release
 mv ~/sui/target/release/sui-node /usr/local/bin/
 mv ~/sui/target/release/sui /usr/local/bin/
 wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
