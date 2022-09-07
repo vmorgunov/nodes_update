e59d40c110f65ea594e1127eeafa598382329f02
Vitalii Morgunov <utlab13@gmail.com>
2022-09-07 17:24:27 +0200

    update sui

--- sui_update.sh
+++ sui_update.sh
@@ -6,10 +6,13 @@ source $HOME/.cargo/env
 cd $HOME
 git clone https://github.com/MystenLabs/sui.git
 cd sui
-git checkout devnet-0.8.0
+git remote add upstream https://github.com/MystenLabs/sui
+git fetch upstream
+git checkout -B devnet --track upstream/devnet
 cargo build -p sui-node -p sui --release
 mv ~/sui/target/release/sui-node /usr/local/bin/
 mv ~/sui/target/release/sui /usr/local/bin/
 wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 systemctl restart suid
+
 echo -e '\n\e[42m SUI was successfully updated \e[0m\n' && sleep 1
