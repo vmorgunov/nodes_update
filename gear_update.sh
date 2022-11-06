c6b7e77322806ea38627c77948475489f32ad3a6
Vitalii Morgunov <utlab13@gmail.com>
2022-11-06 22:28:20 +0100

    Update gear

--- gear_update.sh
+++ gear_update.sh
@@ -2,7 +2,7 @@
 
 sudo systemctl stop gear
 /root/gear-node purge-chain -y
-wget https://builds.gear.rs/gear-nightly-linux-x86_64.tar.xz
+wget https://get.gear.rs/gear-nightly-linux-x86_64.tar.xz
 sudo tar -xvf gear-nightly-linux-x86_64.tar.xz -C /root
 rm gear-nightly-linux-x86_64.tar.xz
 sudo systemctl start gear
