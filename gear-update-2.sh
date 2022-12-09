e7ab2d2406aa579cc8f490a06e4dbb23c0265472
Vitalii Morgunov <utlab13@gmail.com>
2022-12-09 08:02:04 +0100

    Update gear

--- gear-update-2.sh
+++ gear-update-2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 wget https://get.gear.rs/gear-nightly-linux-x86_64.tar.xz
-sudo tar -xvf gear-nightly-linux-x86_64.tar.xz
+sudo tar -xvf gear-nightly-linux-x86_64.tar.xz /root
 rm gear-nightly-linux-x86_64.tar.xz
 sudo systemctl restart gear
