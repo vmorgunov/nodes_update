21b614d177a2febb2299818b055500f07683dd80
Vitalii Morgunov <utlab13@gmail.com>
2023-01-08 08:33:54 +0100

    Update starknet

--- starknet_update.sh
+++ starknet_update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd ~/pathfinder
-
 rustup update
 git fetch
 git checkout v0.4.5
@@ -9,10 +8,8 @@ cargo build --release --bin pathfinder
 mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
 cd py
 source .venv/bin/activate
-#PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
 PIP_REQUIRE_VIRTUALENV=true pip install -e .[dev]
 pip install --upgrade pip
-#cargo run --release --bin pathfinder -- --ethereum.url https://eth-goerli.g.alchemy.com/v2/q9yNqjRYcOxA1X8EUIZJfhD2Rkm-dR-R
 systemctl restart starknetd
 echo -e '\n\e[42m Starknet was successfully updated \e[0m\n' && sleep 1
 pathfinder -V
