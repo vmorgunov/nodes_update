e83546b23b32a80ff1fa1415f035ed8b24ac4f81
Vitalii Morgunov <utlab13@gmail.com>
2023-11-14 17:01:08 +0100

    Update to gemeni-3g-nov-14

--- subspace.sh
+++ subspace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Default variables
-version="gemini-3g-2023-nov-13"
+version="gemini-3g-2023-nov-14"
 function="install"
 # Options
 option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
